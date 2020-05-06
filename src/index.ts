778bcf4944c5ba37988c573d6a389846d58d2865
wujin <wujin@qunhemail.com>
2020-05-06 17:45:47 +0800

    init

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,11 @@
-export * from './hex2rgb';
-export * from './hsv2rgb';
-export * from './rgb2hsv';
+import hex2rgb from './hex2rgb';
+import hsv2rgb from './hsv2rgb';
+import rgb2hex from './rgb2hex';
+import rgb2hsv from './rgb2hsv';
+
+export default {
+  hex2rgb,
+  hsv2rgb,
+  rgb2hex,
+  rgb2hsv
+}
