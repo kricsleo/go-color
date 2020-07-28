b0f569954c098030d3656e52542c62deff3e6e9b
wujin <wujin@qunhemail.com>
2020-07-28 12:35:04 +0800

    feat: add hsl2rgb & rgb2hsl

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,15 @@
 import hex2rgb from './hex2rgb';
 import hsv2rgb from './hsv2rgb';
+import hsl2rgb from './hsl2rgb';
 import rgb2hex from './rgb2hex';
 import rgb2hsv from './rgb2hsv';
+import rgb2hsl from './rgb2hsl';
 
 export {
   hex2rgb,
   hsv2rgb,
+  hsl2rgb,
   rgb2hex,
-  rgb2hsv
+  rgb2hsv,
+  rgb2hsl
 }
