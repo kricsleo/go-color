f4965c50b4df01f17d286ee0d43c14ad2648f340
wujin <wujin@qunhemail.com>
2020-07-29 10:21:50 +0800

    fix: hex移除空格

--- src/hex2rgb.ts
+++ src/hex2rgb.ts
@@ -4,12 +4,13 @@
  * @param hex 十六进制颜色
  */
 export default function hexToRgb(hex: string): number[] {
+  let newHex = hex.trim();
   const shorthandRegex = /^#?([a-f\d])([a-f\d])([a-f\d])$/i;
-  hex = hex.replace(shorthandRegex, function(m, r, g, b) {
+  newHex = newHex.replace(shorthandRegex, function(m, r, g, b) {
     return r + r + g + g + b + b;
   });
 
-  const result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+  const result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(newHex);
   return result
     ? [parseInt(result[1], 16), parseInt(result[2], 16), parseInt(result[3], 16)]
     : null;
